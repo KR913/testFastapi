b24ad429d2c57fce2129b77e6e8391036dc024e9
Tran Thanh Binh (MS/EMC26-XC) <tbr4hc@bosch.com>
2023-10-12 23:49:24 +0700

    prototypes for windows

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from PyQt5 import QtGui
 from PyQt5 import QtCore
 import sys
 import PyQt5.QtWidgets as QtWidgets
+from multiprocessing import Process
 import os
 import subprocess
 import platform
+import uvicorn
+import contextlib
+import time
+import threading
 
-from fastapi import FastAPI
+from fastapi import *
 
 class Task1Tab(QScrollArea):
    def __init__(self, parent = None):
@@ -31,29 +36,29 @@ class Task1Tab(QScrollArea):
       self.setLayout(self.layout)
 
 class Task2Tab(QScrollArea):
-    def __init__(self, parent = None):
-        super(Task2Tab, self).__init__(parent)
+   def __init__(self, parent = None):
+      super(Task2Tab, self).__init__(parent)
 
-        self.setGeometry(100,100,1500,900)
+      self.setGeometry(100,100,1500,900)
         #self.setWindowTitle("PyQt5")
 
-        self.content = QWidget()
-        self.layout = QVBoxLayout(self.content)
-        self.layout.setAlignment(Qt.AlignTop)
+      self.content = QWidget()
+      self.layout = QVBoxLayout(self.content)
+      self.layout.setAlignment(Qt.AlignTop)
 
-        self.title = QLabel()
-        self.title.setText("Task2")
-        self.title.setFont(QFont('Times', 24))
-        self.title.setAlignment(Qt.AlignCenter)
-        self.title.resize(1200,48)
+      self.title = QLabel()
+      self.title.setText("Task2")
+      self.title.setFont(QFont('Times', 24))
+      self.title.setAlignment(Qt.AlignCenter)
+      self.title.resize(1200,48)
 
-        self.actinp = QPushButton()
-        self.actinp.setText("task2")
-        self.actinp.setFont(QFont('Times', 16))
+      self.actinp = QPushButton()
+      self.actinp.setText("task2")
+      self.actinp.setFont(QFont('Times', 16))
 
-        self.layout.addWidget(self.title)
-        self.layout.addWidget(self.actinp)
-        self.setLayout(self.layout)
+      self.layout.addWidget(self.title)
+      self.layout.addWidget(self.actinp)
+      self.setLayout(self.layout)
 
 class MyTabs(QWidget):
    def __init__(self, parent):
@@ -65,8 +70,59 @@ class MyTabs(QWidget):
       self.tab1 = Task2Tab(self)
       self.tabs.addTab(self.tab1,"task2")
       self.layout.addWidget(self.tabs)
+
+      self.actinp = QPushButton()
+      self.actinp.setText("activate fastapi")
+      self.actinp.setFont(QFont('Times', 16))
+      self.actinp.clicked.connect(lambda: self.create_server())
+      self.layout.addWidget(self.actinp)
+
+      self.endbtn = QPushButton()
+      self.endbtn.setText("activate fastapi")
+      self.endbtn.setFont(QFont('Times', 16))
+      self.endbtn.clicked.connect(lambda: self.close_server())
+      self.endbtn.setEnabled(False)
+      self.layout.addWidget(self.endbtn)
+
       self.setLayout(self.layout)
+      self.process = 0
+
+   def create_server(self):
+      print("create server")
+      self.actinp.setEnabled(False)
+      self.endbtn.setEnabled(True)
+      self.process = Process(target=server_test) 
+      self.process.start() 
    
+   def close_server(self):
+      print("close server")
+      self.actinp.setEnabled(True)
+      self.endbtn.setEnabled(False)
+      self.process.terminate() 
+      self.process = 0
+
+
+def server_test():
+   app = FastAPI()
+   @app.post("/uploadfile/")
+   async def create_upload_file(file: UploadFile):
+      fpath = os.path.join(os.getcwd(), file.filename)
+      try:
+         contents = file.file.read()
+         with open(fpath, 'wb') as f:
+            f.write(contents)
+      except Exception:
+         return {"message": "There was an error uploading the file"}
+      finally:
+         file.file.close()
+      return {"message": f"Successfully uploaded {file.filename}"}
+   
+   kwargs={"app": app, "port": 8000}
+   uvicorn.run(**kwargs)
+
+
+#uvicorn.run("main:app", port = 8080, reload = True)
+#config = uvicorn.Config("main:app", host="192.168.1.10", port=8008, log_level="info")
 
 class MainWindow(QMainWindow):
    def __init__(self):
@@ -77,6 +133,10 @@ class MainWindow(QMainWindow):
       self.setCentralWidget(self.table_widget)
       
    def closeEvent(self, Event):
+      print("close")
+      if self.table_widget.process != 0:
+         self.table_widget.process.terminate()
+         print("close and terminated \n")
       return
 
 
@@ -87,13 +147,7 @@ def window():
    w.show()
    sys.exit(app.exec_())
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 if __name__ == '__main__':
-    print("Start")
-    window()
+   print("Start")
+   window()
 
--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ echo conda activated!
 
 call pip3 install -r requirements.txt
 
-call python -m uvicorn main:app --reload --workers 1 --host 10.185.78.54 --port 8008
+call python main.py
 
 call conda deactivate
